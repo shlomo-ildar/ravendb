95206e4baddb5a797bbd9ae07e469a656854b85c
shlomo kazakov <shlomo.kazakov@gmail.com>
2021-11-21 18:41:14 +0200

    fixed Create for JsHandle, refactored CreateMapReduceOutputAccessor

--- src/Raven.Server/Documents/Indexes/Persistence/Lucene/Documents/PropertyAccessor.cs
+++ src/Raven.Server/Documents/Indexes/Persistence/Lucene/Documents/PropertyAccessor.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using Jint;
 using Jint.Native;
 using Jint.Native.Object;
 using Jint.Runtime.Interop;
 using Microsoft.CSharp.RuntimeBinder;
+using Raven.Client.ServerWide.JavaScript;
 using Raven.Server.Extensions;
 using Sparrow.Json;
 using Raven.Server.Config.Categories;
@@ -30,7 +32,7 @@ namespace Raven.Server.Documents.Indexes.Persistence.Lucene.Documents
 
         public static IPropertyAccessor Create(Type type, object instance)
         {
-            if (type == typeof(ObjectInstance) || type == typeof(InternalHandle))
+            if (type == typeof(JsHandle) || type == typeof(ObjectInstance) || type == typeof(InternalHandle))
                 return new JsPropertyAccessor(null);
 
             if (instance is Dictionary<string, object> dict)
@@ -38,16 +40,23 @@ namespace Raven.Server.Documents.Indexes.Persistence.Lucene.Documents
 
             return new PropertyAccessor(type);
         }
-
-        internal static IPropertyAccessor CreateMapReduceOutputAccessor(Type type, object instance, Dictionary<string, CompiledIndexField> groupByFields, bool isObjectInstance = false)
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static IPropertyAccessor CreateMapReduceOutputAccessorJs(Dictionary<string, CompiledIndexField> groupByFields)
         {
-            if (isObjectInstance || (type == typeof(ObjectInstance) || type.IsSubclassOf(typeof(ObjectInstance)))
-                                 || (type == typeof(InternalHandle) && instance is InternalHandle jsInstance && jsInstance.IsObject))
-                return new JsPropertyAccessor(groupByFields);
+            return new JsPropertyAccessor(groupByFields);
+        }
 
+        internal static IPropertyAccessor CreateMapReduceOutputAccessor(object instance, Dictionary<string, CompiledIndexField> groupByFields, bool isObjectInstance = false, [CanBeNull] Type type = null)
+        {
+            if (instance is JsHandle)
+                return CreateMapReduceOutputAccessorJs(groupByFields);
+            
             if (instance is Dictionary<string, object> dict)
                 return DictionaryAccessor.Create(dict, groupByFields);
 
+            if (type == null)
+                type = instance.GetType();
             return new PropertyAccessor(type, groupByFields);
         }
 
@@ -180,21 +189,9 @@ namespace Raven.Server.Documents.Indexes.Persistence.Lucene.Documents
         public IEnumerable<(string Key, object Value, CompiledIndexField GroupByField, bool IsGroupByField)> GetPropertiesInOrder(object target)
         {
 
-            if (target is ObjectInstance oi)
+            if (target is JsHandle jsTarget && jsTarget.IsObject)
             {
-                foreach (var property in oi.GetOwnProperties())
-                {
-                    var propertyAsString = property.Key.AsString();
-
-                    CompiledIndexField field = null;
-                    var isGroupByField = _groupByFields?.TryGetValue(propertyAsString, out field) ?? false;
-
-                    yield return (propertyAsString, GetValue(property.Value.Value), field, isGroupByField);
-                }
-            }
-            else if (target is InternalHandle jsValue && jsValue.IsObject)
-            {
-                foreach (var (propertyName, jsPropertyValue) in jsValue.GetOwnProperties())
+                foreach (var (propertyName, jsPropertyValue) in jsTarget.GetOwnProperties())
                 {
                     using (jsPropertyValue)
                     {
@@ -206,7 +203,7 @@ namespace Raven.Server.Documents.Indexes.Persistence.Lucene.Documents
                 }
             }
             else
-                throw new ArgumentException($"JsPropertyAccessor.GetPropertiesInOrder is expecting a target of type ObjectInstance but got one of type {target.GetType().Name}.");
+                throw new ArgumentException($"JsPropertyAccessor.GetPropertiesInOrder is expecting a target of type JsHandle and IsObject but got one of type {target.GetType().Name}.");
         }
 
         public object GetValue(string name, object target)
@@ -342,6 +339,74 @@ namespace Raven.Server.Documents.Indexes.Persistence.Lucene.Documents
             }
 
             ThrowInvalidObject(new JsHandle(jsValue));
+            return null;
+        }
+
+        private static object GetValue(JsHandle jsValue)
+        {
+            var engineType = jsValue.EngineType;
+            return engineType switch
+            {
+                JavaScriptEngineType.Jint => GetValue(jsValue.Jint.Item),
+                JavaScriptEngineType.V8 => GetValue(jsValue.Jint.Item),
+                _ => throw new NotSupportedException($"Not supported JS engine kind '{engineType}'.")
+            };
+        }
+
+        private static object GetValueUnified(JsHandle jhValue) // we can replace the above three methods with this one but at some cost to performance
+        {
+            var jsValue = jhValue.Handler; // this is with boxing, without boxing but with two step access on each call would be if not using handler: jsValue = jhValue
+            if (jsValue.IsNull)
+                return null;
+            if (jsValue.IsStringEx)
+                return jsValue.AsString;
+            if (jsValue.IsBoolean)
+                return jsValue.AsBoolean;
+            if (jsValue.IsInt32)
+                return jsValue.AsInt32;
+            if (jsValue.IsNumberEx)
+                return jsValue.AsDouble;
+            if (jsValue.IsDate)
+                return jsValue.AsDate;
+
+            if (jsValue.IsArray)
+            {
+                var arrayLength =  jsValue.ArrayLength;
+                var array = new object[arrayLength];
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    using (var jsItem = jsValue.GetProperty(i))
+                        array[i] = GetValue(jsItem);
+                }
+                return array;
+            }
+
+            if (jsValue.IsObject)
+            {
+                var boundObject = jsValue.Object;
+                if (boundObject != null)
+                {
+                    switch (boundObject)
+                    {
+                        case LazyStringValue lsv:
+                            return lsv;
+
+                        case LazyCompressedStringValue lcsv:
+                            return lcsv;
+
+                        case LazyNumberValue lnv:
+                            return lnv; //should be already blittable supported type.
+                    }
+                }
+                return jhValue.Clone();
+            }
+
+            if (jsValue.IsUndefined)
+            {
+                return null;
+            }
+
+            ThrowInvalidObject(jhValue);
             return null;
         }
 
