8f1b10f3bb6fe6a72f145dcb5e2f09de7c86043d
shlomo kazakov <shlomo.kazakov@gmail.com>
2022-03-01 21:15:05 +0200

    fixed missing comma in js script, introduced ordersCount

--- test/SlowTests/Server/Documents/ETL/Olap/S3Tests.cs
+++ test/SlowTests/Server/Documents/ETL/Olap/S3Tests.cs
@@ -528,10 +528,11 @@ loadToOrders(partitionBy(['order_date', key]),
                 using (var store = GetDocumentStore(Options.ForJavaScriptEngine(jsEngineType)))
                 {
                     var baseline = new DateTime(2020, 1, 1).ToUniversalTime();
+                    var ordersCount = 100;
 
                     using (var session = store.OpenAsyncSession())
                     {
-                        for (int i = 0; i < 100; i++)
+                        for (int i = 0; i < ordersCount; i++)
                         {
                             await session.StoreAsync(new Order
                             {
@@ -550,7 +551,7 @@ loadToOrders(partitionBy(['order_date', key]),
                     var script = @"
 loadToOrders(noPartition(),
     {
-        OrderDate : this.OrderedAt
+        OrderDate : this.OrderedAt,
         Company : this.Company,
         ShipVia : this.ShipVia
     });
@@ -585,7 +586,7 @@ loadToOrders(noPartition(),
                                 Assert.True(field.Name.In(expectedFields));
 
                                 var data = rowGroupReader.ReadColumn((DataField)field).Data;
-                                Assert.True(data.Length == 100);
+                                Assert.True(data.Length == ordersCount);
 
                                 if (field.Name == ParquetTransformedItems.LastModifiedColumn)
                                     continue;
